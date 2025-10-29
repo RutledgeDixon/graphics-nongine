44ad191e9bae3c4049a0da205619a92365e431b8
Rutledge Dixon <rutledgedixon@letu.edu>
2025-10-28 21:17:37 -0500

    updated shaders to use 3.0 instead of a mix between 1.0 and 3.0

--- Code/Models/shaders/default.fs
+++ Code/Models/shaders/default.fs
@@ -3,13 +3,13 @@ precision mediump float;
 out vec4 fragColor;
 uniform vec3 uTint;
 
-//varying vec4 fColor;
-varying  vec2 fTexCoord;
+//in vec4 fColor;
+in vec2 fTexCoord;
 
 uniform sampler2D texture;
 
 void main(void) {
 	//fragColor = vec4(uTint, 1.0);
-	//fragColor = texture2D(texture, fTexCoord) * fColor * vec4(uTint, 1.0);
-	fragColor = texture2D(texture, fTexCoord) * vec4(uTint, 1.0);
+	//fragColor = texture(texture, fTexCoord) * fColor * vec4(uTint, 1.0);
+	fragColor = texture(texture, fTexCoord) * vec4(uTint, 1.0);
 }
